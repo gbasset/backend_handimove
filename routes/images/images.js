f7a86a2764781922e61cfa2cd72fa3969f78c4e1
gaetan <gbassetprotonmail.com>
2020-11-14 19:16:41 +0100

    Backend images add upload for the avatars and get the avatars

--- routes/images/images.js
+++ routes/images/images.js
@@ -5,24 +5,7 @@ const multer = require("multer");
 const upload = multer({ dest: "public/" });
 const fs = require("fs");
 
-// router.post('/uploaddufichier', upload.single('monfichier'), function (req, res, next) {
-//     var tmp_path = req.file.path;
-//     console.log(tmp_path);
-//     fs.rename(req.file.path, 'public/images/' + req.file.originalname, function (err) {
-//         if (err) {
-//             res.send('problème durant le déplacement');
-//         } else {
-//             res.send('Fichier uploadé avec succès');
-//         }
-//     });
-// })
-
-// faire un upload pour le site en mode images 
-// faire un upload par event 
-// faire un get par etablissement 
-// get par event 
-
-router.post("/upload/:id", upload.array("file"), (req, res, next) => {
+router.post("/upload/establish/:id", upload.array("file"), (req, res, next) => {
     const id = req.params.id;
     let error = false;
     req.files.map(file => {
@@ -69,4 +52,120 @@ router.post("/upload/:id", upload.array("file"), (req, res, next) => {
         return res.send("Files uploaded sucessfully").status(200);
 });
 
+//  get establishment by region by name 
+router.route(['/establishment/:id'])
+    .get(function (req, res) {
+        connection.query(`Select * from images i join images_establishment ies on i.id=ies.id_image where ies.id_esta=${req.params.id}`, (err, results) => {
+            if (err) {
+                res.status(500).send("Erreur lors de la récupération des etablissements");
+                console.log('Query error: ' + err);
+            } else {
+                res.json(results).status(200);
+            }
+        })
+    })
+router.post("/upload/event/:id", upload.array("file"), (req, res, next) => {
+    const id = req.params.id;
+    let error = false;
+    req.files.map(file => {
+        console.log(file);
+        let arr = []
+        let Timestamp = Math.round(new Date().getTime() / 1000)
+        let FileName = file.originalname
+        let regex1 = /\’\”\;\,\*\./gi;
+        let NewFileName = FileName.replace(regex1, "").split(" ").join("").toLowerCase()
+        fs.rename(file.path, `public/${Timestamp}${NewFileName}`, err => {
+            if (err) {
+                error = true;
+            } else {
+                const objectFile = {
+                    image_name: `${Timestamp}${NewFileName}`,
+                    image_url: `public/${Timestamp}${NewFileName}`,
+                }
+                connection.query("INSERT INTO images SET ?", objectFile, (err2, results) => {
+                    if (err2) {
+                        error = true;
+                    } else {
+                        console.log(results);
+                        connection.query(`INSERT INTO images_event (id_event,id_image) VALUES (${id}, ${results.insertId})`, (err3, results) => {
+                            if (err3) {
+                                error = true;
+                            } else {
+                                connection.query(`Select * from images i join images_event ies on i.id=ies.id_image where ies.id_image=${id}`, (err4, results) => {
+                                    if (err4) {
+                                        error = true;
+                                    } else {
+                                        console.log("results", results);
+                                    }
+                                })
+                            }
+                        })
+                    }
+                })
+            }
+        })
+    })
+    if (error) {
+        res.send("Problem when uploading files").status(500);
+    } else
+        return res.send("Files uploaded sucessfully").status(200);
+});
+
+//  get establishment by region by name 
+router.route(['/event/:id'])
+    .get(function (req, res) {
+        connection.query(`Select * from images i join images_event ies on i.id=ies.id_image where ies.id_event=${req.params.id}`, (err, results) => {
+            if (err) {
+                res.status(500).send("Erreur lors de la récupération des etablissements");
+                console.log('Query error: ' + err);
+            } else {
+                res.json(results).status(200);
+            }
+        })
+    })
+
+/// post an avatar image 
+router.post("/upload/avatar/", upload.array("file"), (req, res, next) => {
+    let error = false;
+    req.files.map(file => {
+        console.log(file);
+        let arr = []
+        let Timestamp = Math.round(new Date().getTime() / 1000)
+        let FileName = file.originalname
+        let regex1 = /\’\”\;\,\*\./gi;
+        let NewFileName = FileName.replace(regex1, "").split(" ").join("").toLowerCase()
+        fs.rename(file.path, `public/${Timestamp}${NewFileName}`, err => {
+            if (err) {
+                error = true;
+            } else {
+                const objectFile = {
+                    name: `${Timestamp}${NewFileName}`,
+                    url: `public/${Timestamp}${NewFileName}`,
+                }
+                connection.query("INSERT INTO avatars SET ?", objectFile, (err2, results) => {
+                    if (err2) {
+                        error = true;
+                    }
+                })
+            }
+        })
+    })
+    if (error) {
+        res.send("Problem when uploading files").status(500);
+    } else
+        return res.send("Files uploaded sucessfully").status(200);
+});
+
+//  get all avatars
+router.route(['/avatars'])
+    .get(function (req, res) {
+        connection.query(`Select * from avatars`, (err, results) => {
+            if (err) {
+                res.status(500).send("Erreur lors de la récupération des avatars");
+                console.log('Query error: ' + err);
+            } else {
+                res.json(results).status(200);
+            }
+        })
+    })
 module.exports = router
