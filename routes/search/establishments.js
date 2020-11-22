be5a2392a37f91e7a4319a6439132dbeefcafaa5
gaetan <gbassetprotonmail.com>
2020-11-23 00:06:35 +0100

    add ascending search and groupe by name for establishment search

--- routes/search/establishments.js
+++ routes/search/establishments.js
@@ -17,7 +17,7 @@ router.route(['/establishments/:id'])
 //  get establishment by region name 
 router.route(['/establishment/town/:request'])
     .get(function (req, res) {
-        connection.query(`SELECT e.name,e.town, e.id_etablishment, e.address, e.url_website, e.phone, e.zip_code, e.region, e.department, e.category, e.activity,e.handicaps,e.handicaps, t.latitude, t.longitude FROM town t INNER join establishment e on t.name=e.town WHERE e.town LIKE "${req.params.request}"`, (err, results) => {
+        connection.query(`SELECT e.name,e.town, e.id_etablishment, e.address, e.url_website, e.phone, e.zip_code, e.region, e.department, e.category, e.activity,e.handicaps,e.handicaps, t.latitude, t.longitude FROM town t INNER join establishment e on t.name=e.town WHERE e.town LIKE "${req.params.request}" group by e.id_etablishment ORDER BY e.name ASC`, (err, results) => {
             if (err) {
                 res.status(500).send("Erreur lors de la récupération de l'etablissement");
                 console.log('Query error: ' + err);
@@ -29,7 +29,7 @@ router.route(['/establishment/town/:request'])
 //  get establishment by region by name 
 router.route(['/establishment/regions/:request'])
     .get(function (req, res) {
-        connection.query(`SELECT e.name, e.town,e.id_etablishment, e.address, e.url_website, e.phone, e.zip_code, e.region, e.department, e.category, e.activity,e.handicaps,e.handicaps, t.latitude, t.longitude FROM town t INNER join establishment e on t.name=e.town WHERE e.region LIKE "${req.params.request}"`, (err, results) => {
+        connection.query(`SELECT e.name, e.town,e.id_etablishment, e.address, e.url_website, e.phone, e.zip_code, e.region, e.department, e.category, e.activity,e.handicaps,e.handicaps, t.latitude, t.longitude FROM town t INNER join establishment e on t.name=e.town WHERE e.region LIKE "${req.params.request}" group by e.id_etablishment ORDER BY e.town ASC`, (err, results) => {
             if (err) {
                 res.status(500).send("Erreur lors de la récupération des etablissements");
                 console.log('Query error: ' + err);
@@ -41,7 +41,7 @@ router.route(['/establishment/regions/:request'])
 //  get establishment by region by name 
 router.route(['/establishment/department/:request'])
     .get(function (req, res) {
-        connection.query(`SELECT  e.name,e.town, e.id_etablishment, e.address, e.url_website, e.phone, e.zip_code, e.region, e.department, e.category, e.activity,e.handicaps,e.handicaps, t.latitude, t.longitude FROM town t INNER join establishment e on t.name=e.town WHERE e.department LIKE"${req.params.request}"`, (err, results) => {
+        connection.query(`SELECT  e.name,e.town, e.id_etablishment, e.address, e.url_website, e.phone, e.zip_code, e.region, e.department, e.category, e.activity,e.handicaps,e.handicaps, t.latitude, t.longitude FROM town t INNER join establishment e on t.name=e.town WHERE e.department LIKE"${req.params.request}" group by e.id_etablishment ORDER BY e.town ASC`, (err, results) => {
             if (err) {
                 res.status(500).send("Erreur lors de la récupération des etablissements");
                 console.log('Query error: ' + err);
